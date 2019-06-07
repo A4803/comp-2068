1755191c811306f458c0d38ad3970d6fcdd0cbcc
A4803 <alay15051996@gmail.com>
2019-06-07 19:41:46 -0400

    part-02 done

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -7,11 +7,29 @@ exports.new = (req, res) => {
 };
 
 exports.index = (req, res) => {
-	
+	Blog.find()
+    .then(blogs => {
+      res.render('blogs/index', {
+        blogs: blogs,
+        title: 'Archive'
+      });
+    })
+    .catch(err => {
+      console.log(`ERROR: ${err}`);
+    });
 };
 
 exports.show = (req, res) => {
-	
+	Blog.findById(req.params.id)
+    .then(blog => {
+      res.render('blogs/show', {
+        blog: blog,
+        title: blog.title
+      });
+    })
+    .catch(err => {
+      console.log(`ERROR: ${err}`);
+    });
 };
 
 exports.create = (req, res) => {
@@ -27,9 +45,28 @@ exports.create = (req, res) => {
 
 exports.drafts = (req, res) => {
 
+Blog.find().drafts()
+    .then(drafts => {
+      res.render('blogs/index', {
+        blogs: drafts,
+        title: 'Drafts'
+      });
+    })
+    .catch(err => {
+      console.log(`ERROR: ${err}`);
+    });
 };
 
 
 exports.published = (req, res) => {
-
+Blog.find().published()
+    .then(published => {
+      res.render('blogs/index', {
+        blogs: published,
+        title: 'Published'
+      });
+    })
+    .catch(err => {
+      console.log(`ERROR: ${err}`);
+    });
 };
