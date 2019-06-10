436cbacd9f72e739266e5120e97d5ea06bb8695c
A4803 <alay15051996@gmail.com>
2019-06-10 16:14:19 -0400

    Done with Publish and Darft

--- routes/blogs.js
+++ routes/blogs.js
@@ -8,5 +8,9 @@ router.get(`/drafts`, BlogsController.drafts);
 router.get(`/published`, BlogsController.published);
 router.get(`/:id`, BlogsController.show);
 router.post(`/`, BlogsController.create);
+router.get(`/:id/edit`, BlogsController.edit);
+router.post(`/update`, BlogsController.update);
+router.post(`/destroy`, BlogsController.destroy);
+
 
 module.exports = router;
